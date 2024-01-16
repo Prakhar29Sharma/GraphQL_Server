26e1b16b603d5a157ca9911e079bcf5d6748c06f
Prakhar Sharma <prakhar29sharma@gmail.com>
2024-01-16 21:20:50 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// db
+import db from './db.js';
+
+// types
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        game(_, args) {
+            return db.games.find((game) => game.id === args.id)
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        },
+        author(_, args) {
+            return db.authors.find((author) => author.id === args.id)
+        },
+        review(_, args) {
+            return db.reviews.find((review) => review.id === args.id)
+        }
+    }
+}
+
+// server setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+})
+
+console.log('Server ready at port ', 4000)
